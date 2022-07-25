6046a2ef1fa6b9f14dc354b760fce5b567559a8e
santhosh <santhosh963vully@gmail.com>
2022-07-25 14:02:17 +0530

    ec2-commit02

--- c2-ec2instance.tf
+++ c2-ec2instance.tf
@@ -1,12 +1,13 @@
 # Resource: EC2 Instance
-resource "aws_instance" "myec2vm" {
-  ami = "ami-0533f2ba8a1995cf9"
-  instance_type = "t3.micro"
-  user_data = file("${path.module}/app1-install.sh")
-  tags = {
-    "Name" = "EC2 Demo terraform"
-  }
+resource "aws_instance" "myec2-vm" {
+    ami = ami-0c802847a7dd848c0
+    instance_type = t2.micro
+    user_data = file("${path.module}/app1-install.sh")
+    tags = {
+        name = ec2-demo 
+    }
 }
 
 
 
+ 
