c61ca6c2f6af2c4e536e163161741802d129fc5d
santhosh <santhosh963vully@gmail.com>
2022-07-25 14:40:04 +0530

    ec2-commit5

--- c2-ec2instance.tf
+++ c2-ec2instance.tf
@@ -1,5 +1,5 @@
 # Resource: EC2 Instance
-resource "aws_instance" "myec2-vm" {
+resource "aws_instance" "myec2vm" {
     ami = "ami-0c802847a7dd848c0"
     instance_type = "t2.micro"
     user_data = file("${path.module}/app1-install.sh")
