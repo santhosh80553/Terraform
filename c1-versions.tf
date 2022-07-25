2a84f2b25e208ed6906ae35b58502c0502f9eb71
santhosh <santhosh963vully@gmail.com>
2022-07-25 12:54:14 +0530

    commit01

---
+++ c1-versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = ap-southeast-1
+}
